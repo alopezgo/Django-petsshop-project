e14980f517ad11f4efe6067184f059919c79c509
Andrea Lopez <alopezgo1991@gmail.com>
2022-06-13 23:18:33 -0400

    Serializer Productos

--- rest_productos/serializers.py
+++ rest_productos/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from core.models import Producto
 
 class ProductoSerializer(serializers.ModelSerializer):
+    
+    desc_categoria =  serializers.CharField(source="categoria")
     class Meta:
         model= Producto
-        fields = '__all__'
+        fields = ['idproducto', 'marca', 'desc_categoria', 'descripcion','imagen']
